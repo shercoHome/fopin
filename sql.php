763d18aa7c310303c20d3c0b823558ee70c5d1b3
s <sherco.home@gmail.com>
2019-07-28 15:21:23 +0800

    Update sql.php

--- sql.php
+++ sql.php
@@ -1,8 +1,8 @@
 <?php
 date_default_timezone_set("Asia/Chongqing");
-//$servername = "localhost";$username = "root";$password = "root";$dbname = "fopindaquan_utf8_general_ci";$dbname = "hdm189315162_db";
+//$servername = "localhost";$username = "root";$password = "root";$dbname = "****";$dbname = "hdm189315162_db";
 
-$servername="hdm189315162.my3w.com";$username="hdm189315162";$password="Nevermore123"; $dbname="hdm189315162_db";
+$servername="hdm189315162.my3w.com";$username="****";$password="****"; $dbname="****";
 
 // 设置编码，防止中文乱码//mysqli_query($conn , "set names utf8");
 ?>
